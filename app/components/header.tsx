4e08b4762a3ab2980eb4be583b3ebc7b1dd2a9dd
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-09-11 17:05:36 +0200

    shopify nextjs

---
+++ app/components/header.tsx
@@ -0,0 +1,22 @@
+const header = () => {
+    const navigation = [
+        {
+            name: 'Home',
+            href: '/'
+        },
+        {
+            name: 'Shop',
+            href: '/shop'
+        }
+    ]
+    return ( 
+        <div className="py-10 flex justify-between">
+            <h2 className="font-bold uppercase text-3xl">baby boomer</h2>
+            <div>
+
+            </div>
+        </div>
+     );
+}
+ 
+export default header;
