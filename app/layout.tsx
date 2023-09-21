f8803b585ef33926ed820675ba30a8f4486da25d
Marvey1203 <102023269+Marvey1203@users.noreply.github.com>
2023-09-21 18:46:00 +0200

    header and footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Header from './components/Header'
-import Footer from './components/Footer'
+import header from './components/header'
+import footer from './components/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,9 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <div className='w-3/4 mx-auto'>
-          <Header/>
+          <header/>
           {children}
-          <Footer/>
+          <footer/>
         </div>
         
       </body>
